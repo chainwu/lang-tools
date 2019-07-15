be358d03430fff9f5dba59473acab4a77663d413
Lee.Chain-Wu 李謙吾 TAO <Lee.Chain-Wu@inventec.com>
2019-07-15 05:00:35 +0100

    change output format

--- xml-parse.sh
+++ xml-parse.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <file>"
+    exit -1
+fi
+
 xmllint --xpath "string(//text)" $1 |sed "s/[(][^)]*[)]\|，\|「\|」\|。\|：\|．\|；\|？\|！\|—\|【\|】\|《\|》\|"+vrv"\|"+vrr"\|"+spo"\|"+spv"\|（\|）\|\[\|\]\|、\|,\|\.\|:\|;\|\?\|\!\|\"\|\%\|'∥'//g" |  sed -e 's/^[ \t　]*//' |sed -e 's/　/ /g'|sed '/^$/d'> t1.txt
 
 xmllint --xpath "string(//text)" $1 |sed "s/，\|。\|：\|；\|？\|！\|（\|）\|、\|,\|\.\|:\|;\|\?\|\!\|\"\|\%\-//g"|sed -e 's/(COLONCATEGORY)\|(PAUSECATEGORY)\|(PERIODCATEGORY)\|(PARENTHESISCATEGORY)\|(QUESTIONCATEGORY)\|(COMMACATEGORY)\|(EXCLANATIONCATEGORY)\|(SEMICOLONCATEGORY)\|(DASHCATEGORY)\|(SPCHANGECATEGORY)//g'|perl -ne 'printf "%s\n", join(" " , $_ =~ /["(].*?[)"]/g)'|sed -e 's/(//g'|sed -e 's/)//g'|sed '/^$/d'> t2.txt
