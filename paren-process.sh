ff0d6d8ec4f788f09d652496cbd1337b60d3e255
Lee.Chain-Wu 李謙吾 TAO <Lee.Chain-Wu@inventec.com>
2019-07-16 08:20:47 +0100

    fix encoding

---
+++ paren-process.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <file>"
+    exit -1
+fi
+
+ENCODING=`file -i examp.txt |awk '{print $3}'|sed 's/charset=//'`
+#echo $ENCODING
+
+if [ $ENCODING != "UTF-8" ]; then
+    iconv -f BIG5 -t UTF-8 $1 > tmpfile.txt
+    PROCFILE=tmpfile.txt 
+else
+    PROCFILE=$1 
+fi
+
+#echo $PROCFILE 
+cat $PROCFILE | sed -e "s/\r\n$/\n/" | sed -e 's/(COLONCATEGORY)\|(PAUSECATEGORY)\|(PERIODCATEGORY)\|(PARENTHESISCATEGORY)\|(QUESTIONCATEGORY)\|(COMMACATEGORY)\|(EXCLANATIONCATEGORY)\|(SEMICOLONCATEGORY)\|(DASHCATEGORY)\|(SPCHANGECATEGORY)//g' | sed -e "s/(/ /g" | sed "s/)/\n/g" | sed "s/-\|，\|「\|」\|。\|•\|：\|．\|；\|？\|！\|—\|【\|】\|《\|》\|"+vrv"\|"+vrr"\|"+spo"\|"+spv"\|（\|）\|\[\|\]\|、\|,\|\.\|:\|;\|\?\|\!\|\"\|\%\|'∥'//g" | sed -e "s/^[ 　\t]*//" | sed -e "s/\r//g"| sed -e '/^$/N;/^\n$/D'
